468b700160132237855301a7b1e5e34339664014
Timothy Odebunmi <dev.brymes@gmail.com>
2022-02-02 04:57:45 +0100

    Change Variables to Camel Casee

--- utils/constants.go
+++ utils/constants.go
@@ -2,11 +2,12 @@ package utils
 
 import "fmt"
 
-const API_URL = "https://api.lazerpay.engineering/api/v1"
+const ApiUrl = "https://api.lazerpay.engineering/api/v1"
 
-var API_URL_INIT_TRANSACTION = fmt.Sprintf("%v/transaction/initialize", API_URL)
+var ApiUrlInitTransaction = fmt.Sprintf("%v/transaction/initialize", ApiUrl)
 
-var API_URL_CONFIRM_TRANSACTION = fmt.Sprintf("%v/transaction/verify", API_URL)
+var ApiUrlConfirmTransaction = fmt.Sprintf("%v/transaction/verify", ApiUrl)
 
-var API_URL_GET_ACCEPTED_COINS = fmt.Sprintf("%v/coins", API_URL)
-var API_URL_TRANSFER_FUNDS = fmt.Sprintf("%v/transfer", API_URL)
+var ApiUrlGetAcceptedCoins = fmt.Sprintf("%v/coins", ApiUrl)
+
+var ApiUrlTransferFunds = fmt.Sprintf("%v/transfer", ApiUrl)
