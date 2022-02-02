499709c633d321cad78689b61fa05ada6b4aacfb
Timothy Odebunmi <dev.brymes@gmail.com>
2022-02-02 04:58:06 +0100

    Created Important Types

---
+++ utils/types.go
@@ -0,0 +1,20 @@
+package utils
+
+type TransactionPayloadData struct {
+	Amount        string
+	Coin          string
+	Currency      string
+	CustomerEmail string
+	CustomerName  string
+}
+
+type ConfirmTransactionPayloadData struct {
+	address string
+}
+
+type TransferFundsPayloadData struct {
+	Amount     string
+	Recipient  string
+	Coin       string
+	Blockchain string
+}
