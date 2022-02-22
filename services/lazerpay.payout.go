9c53ba7f8ba2a3641a8b55a7e2d687092253d32b
Timothy Odebunmi <dev.brymes@gmail.com>
2022-02-22 23:32:31 +0100

    Transfer Funds

---
+++ services/lazerpay.payout.go
@@ -0,0 +1,33 @@
+package services
+
+import (
+	u "LazerPay-Go-SDK/utils"
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+type TransferFunds struct {
+	Amount     int `json:"amount"`
+	Recipient  string `json:"recipient"`
+	Coin       string `json:"coin"`
+	Blockchain string `json:"blockchain"`
+}
+
+func (tf *TransferFunds) Transfer(keys u.ApiKeys) (u.TransferResponse, error) {
+	var data u.TransferResponse
+
+	url := u.ApiUrlTransferFunds
+	payload, err := json.Marshal(&tf)
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payload))
+
+	resp := u.MakeReq(*req, keys)
+
+	err = json.Unmarshal(resp, &data)
+	if err != nil {
+		return data, err
+	}
+
+	return data, err
+}
--- utils/responseTypes.go
+++ utils/responseTypes.go
@@ -19,3 +19,19 @@ type AcceptedCoins struct {
 	Status     string `json:"status"`
 	StatusCode int    `json:"statusCode"`
 }
+
+type TransferResponse struct {
+	Message string `json:"message"`
+	Error   string `json:"error"`
+	Status  string `json:"status"`
+	Data    struct {
+		Id              string    `json:"id"`
+		CreatedAt       time.Time `json:"createdAt"`
+		UpdatedAt       time.Time `json:"updatedAt"`
+		TransactionHash string    `json:"transactionHash"`
+		WalletAddress   string    `json:"walletAddress"`
+		Coin            string    `json:"coin"`
+		Amount          int       `json:"amount"`
+	} `json:"data"`
+	StatusCode int `json:"statusCode"`
+}
