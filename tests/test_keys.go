158816ac407b0758831cab6c5f051771a62a82f8
Timothy Odebunmi <dev.brymes@gmail.com>
2022-02-26 07:58:36 +0100

    Move Test Keys

---
+++ tests/test_keys.go
@@ -0,0 +1,8 @@
+package tests
+
+import "LazerPay-Go-SDK/utils"
+
+var testKeys = utils.ApiKeys{
+	PubKey: "pk_live_0N24k7lsrr7NGfrDQpIjPGy9z61LkXjUqxX3r99XblXHemwMht",
+	SecKey: "pk_test_QVaSyqlCqhpY585oHV3hc01nfMZfOzhy5YpPIOc0LywoWK4EVt",
+}
