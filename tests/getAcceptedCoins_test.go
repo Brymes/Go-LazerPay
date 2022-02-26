337fb36d5a612986a2fd89a56a2dc1e292feff6d
Timothy Odebunmi <dev.brymes@gmail.com>
2022-02-26 07:34:15 +0100

    Get Accepted Coins Test

---
+++ tests/getAcceptedCoins_test.go
@@ -0,0 +1,15 @@
+package tests
+
+import (
+	"LazerPay-Go-SDK/services"
+	"testing"
+)
+
+func TestAcceptedCoins(t *testing.T) {
+	testCase := services.GetAcceptedCoins{}
+
+	res, err := testCase.GetCoins(testKeys)
+	if err != nil {
+		t.Errorf("Test: Get All Coins failed: \n Error was: %v \n Data Returned is: %v", err, res)
+	}
+}
